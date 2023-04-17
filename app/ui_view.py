2cc7cd6c14e6aec04aef63456e8e7db26be6209b
NSUSpray <nsu.spray@gmail.com>
2023-04-17 16:53:22 +0600

    Fix #7 Add zoom and panning via mouse wheel and keyboard

--- app/ui_view.py
+++ app/ui_view.py
@@ -3,14 +3,46 @@ from PyQt5.QtWidgets import QGraphicsView, QFrame
 from PyQt5.QtGui import QPainter
 
 
+class GraphicsView(QGraphicsView):
+
+    def wheelEvent(self, event):
+        """https://stackoverflow.com/questions/19113532"""
+        self.setTransformationAnchor(self.NoAnchor)
+        pos = self.mapToScene(event.pos())
+        self.translate(pos.x(), pos.y())
+        zoom_factor = 1.0015**event.angleDelta().y()
+        self.scale(zoom_factor, zoom_factor)
+        self.translate(-pos.x(), -pos.y())
+
+    def keyPressEvent(self, event):
+        if event.key() in {Qt.Key_Minus, Qt.Key_Equal}:
+            self.setTransformationAnchor(self.NoAnchor)
+            center = (self.width() / 2, self.height() / 2)
+            pos = self.mapToScene(*center)
+            self.translate(pos.x(), pos.y())
+            zoom_factor = 1.2
+            if event.key() == Qt.Key_Minus:
+                zoom_factor = 1 / zoom_factor
+            self.scale(zoom_factor, zoom_factor)
+            self.translate(-pos.x(), -pos.y())
+        else:
+            super().keyPressEvent(event)
+
+
 class Ui_MainWindow:
 
     def __init__(self):
-        view = QGraphicsView()
+        view = GraphicsView()
         view.setBackgroundBrush(Qt.gray)
         view.setRenderHint(QPainter.Antialiasing)
         view.setFrameShape(QFrame.NoFrame)
-        view.setSceneRect(-1, -1, 2, 2)  # always centered
+        width = view.viewport().width() * 5
+        height = view.viewport().height() * 5
+        view.setSceneRect(  # always centered
+            -width/2, -height/2, width, height
+        )
+        view.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        view.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.setCentralWidget(view)
         self._graphics_view = view
         self.setWindowTitle('Rigid Packer')
